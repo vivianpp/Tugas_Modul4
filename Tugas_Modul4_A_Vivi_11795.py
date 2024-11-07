6cbbe283afd02da4724994ffeba93fbd9cbd1c4b
vivianpp <220711795@students.uajy.ac.id>
2024-11-07 15:33:31 +0700

    Update Tugas_Modul4_A_Vivi_11795.py

--- Tugas_Modul4_A_Vivi_11795.py
+++ Tugas_Modul4_A_Vivi_11795.py
@@ -46,11 +46,10 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - Vivi</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Users\ASUS\Documents\Tugas4_A_11795'
     model_path = {
-        "AGG_model": os.path.join(model_directory, r'AGG_model.pkl'),
-        "KMeans_model": os.path.join(model_directory, r'KMeans_model.pkl'),
-        "DBSCAN_model": os.path.join(model_directory, r'DBSCAN_model.pkl')
+        "AGG_model": r'AGG_model.pkl',
+        "KMeans_model": r'KMeans_model.pkl',
+        "DBSCAN_model": r'DBSCAN_model.pkl'
     }
 
     models = {}
